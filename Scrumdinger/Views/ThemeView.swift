3f94ea5b61758922acf8936212e8e5cb936134ba
jke <constantinekulak@proton.me>
2023-06-25 12:56:13 +0300

    created ThemeView

---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+
+struct ThemeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+struct ThemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemeView()
+    }
+}
